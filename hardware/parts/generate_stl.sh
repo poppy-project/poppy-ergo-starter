9b590229bd10d1fe6745977da812061d1611c881
Jonathan Grizou <jonathan.grizou@gmail.com>
2015-05-09 16:23:26 +0200

    generate_stl script now automatically detect .scad files and generate them

--- hardware/parts/generate_stl.sh
+++ hardware/parts/generate_stl.sh
@@ -15,21 +15,14 @@ scadArg="\$fn=$fn"
 # create the stl folder if it does not exist
 mkdir -p stl;
 
-# should be automated
-echo "Generating base..."
-openscad -o stl/base.stl base.scad -D $scadArg
+# list all scad file in folder
+scadFileAvailable=$(find . -name "*.scad" -maxdepth 1)
 
-echo "Generating side_to_side..."
-openscad -o stl/side_to_side.stl side_to_side.scad -D $scadArg
-
-echo "Generating U_horn_to_horn..."
-openscad -o stl/U_horn_to_horn.stl U_horn_to_horn.scad -D $scadArg
-
-echo "Generating horn_to_horn..."
-openscad -o stl/horn_to_horn.stl horn_to_horn.scad -D $scadArg
-
-echo "Generating three_ollo..."
-openscad -o stl/three_ollo.stl three_ollo.scad -D $scadArg
-
-echo "Generating pen_holder..."
-openscad -o stl/pen_holder.stl pen_holder.scad -D $scadArg
+# generate all filename.scad files and output stls in ./stl/filename.stl
+for i in $scadFileAvailable; do
+  scadFilename=${i#*/}
+  filename=${scadFilename%.scad}
+  stlFilename="stl/$filename.stl"
+  echo "Generating $filename..."
+  openscad -o $stlFilename $scadFilename -D $scadArg
+done
