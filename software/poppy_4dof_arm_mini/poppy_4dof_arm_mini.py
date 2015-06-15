e2ee642d99e79b7b965ad53c5e57f0814061fdec
juju <julien.jehl@free.fr>
2015-06-15 18:30:50 +0200

    minimum files for instanciate the robot

---
+++ software/poppy_4dof_arm_mini/poppy_4dof_arm_mini.py
@@ -0,0 +1,8 @@
+from poppy.creatures import AbstractPoppyCreature
+
+
+
+class Poppy4dofarmmini(AbstractPoppyCreature):
+    @classmethod
+    def setup(cls, robot):
+        a = 1
---
+++ software/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+
+def version():
+    with open('poppy_4dof_arm_mini/_version.py') as f:
+        return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name='poppy-4dof-arm-mini',
+      version=version(),
+      packages=find_packages(),
+
+      install_requires=['pypot >= 2.2', 'poppy-creature'],
+
+      setup_requires=['setuptools_git >= 0.3', ],
+
+      include_package_data=True,
+      exclude_package_data={'': ['README', '.gitignore']},
+
+      zip_safe=False,
+
+      author='Pierre Rouanet, Jonathan Grizou, Matthieu Lapeyre',
+      author_email='pierre.rouanet@gmail.com',
+      description=' Poppy 4 dof arm mini Software Library',
+      url='https://github.com/poppy-project/poppy-4dof-arm-mini',
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
+
+      **extra
+      )
