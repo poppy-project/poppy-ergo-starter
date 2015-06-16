358d57aeb6244faf3a68ce870f2ab9c53fab1151
juju <julien.jehl@free.fr>
2015-06-16 11:35:39 +0200

    setup of compliance

--- software/poppy_dof4_arm_mini/poppy_dof4_arm_mini.py
+++ software/poppy_dof4_arm_mini/poppy_dof4_arm_mini.py
@@ -4,6 +4,12 @@ from poppy.creatures import AbstractPoppyCreature
 class PoppyDof4ArmMini(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
-        #robot.attach_primitive(Jump(robot), 'jump')
-        pass
+        for m in robot.motors:
+		    m.goto_behavior = 'minjerk'
+
+        for m in robot.motors:
+            m.compliant = False
+			
+		#robot.attach_primitive(Jump(robot), 'jump')
+		print "robot is setup"
         
