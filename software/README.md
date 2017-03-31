ea7c0b3ffa682f38f59eaffd1964763e4676f964
Damien Caselli <damien.caselli@gmail.com>
2017-03-31 17:19:58 +0200

    Add conda package scaffold
    Some files were moved to match poppy-ergo-jr file structure.

--- software/README.md
+++
@@ -1,2 +0,0 @@
-# poppy-creature-mini-template
-This is the basics required to create a poppy mini creature
--- software/poppy_ergo_starter/poppy_ergo_starter.py
+++ software/poppy_ergo_starter/poppy_ergo_starter.py
@@ -6,7 +6,3 @@ class PoppyErgoStarter(AbstractPoppyCreature):
     def setup(cls, robot):
         for m in robot.motors:
             m.goto_behavior = 'dummy'
-
-        #robot.attach_primitive(Jump(robot), 'jump')
-        #print "robot is setup"
-        
